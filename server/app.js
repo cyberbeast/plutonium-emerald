9c818658b36b050d349bc89de34fc83a6c6a20fe
cyberbeast <sandeshgade@gmail.com>
2016-07-03 00:42:14 +0530

    Prototype Scaffolding

--- server/app.js
+++ server/app.js
@@ -2,14 +2,11 @@ var express = require('express');
 // var path = require('path');
 var app = express();
 
-// app.use(express.static('node_modules'));
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('../client/public'));
-app.use(express.static('../client/src'));
+app.use(express.static('../client/dist'));
 
 app.get('/', function(req, res){
     // console.log("Received Request");
-    res.sendFile('../client/src/index.html');
+    res.sendFile('../client/dist/index.html');
 });
 
 app.listen(3005, function(){
