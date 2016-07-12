81dde887929f915c56a78ccc685fc2856ac2a304
cyberbeast <sandeshgade@gmail.com>
2016-07-12 21:08:25 +0530

    Directory Management

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,12 +2,14 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
 import { HTTP_PROVIDERS } from '@angular/http';
+import { appRouterProviders } from './app/app.routes';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrap(AppComponent, [
+    appRouterProviders,
     HTTP_PROVIDERS
 ]);
 
